f2912ed951b1dd27a4bd40d96ad929f894b1e24d
Ricky Raykhenberg <ricky@optimizely.com>
2019-02-07 13:44:14 -0800

    Set up static version of demo page.

---
+++ precache-manifest.9a6450e616ad32900a28d00f05480451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e5575a7520a3a7374852",
+    "url": "/static/js/main.e5575a75.chunk.js"
+  },
+  {
+    "revision": "f0756375c976cb6ccb30",
+    "url": "/static/js/1.f0756375.chunk.js"
+  },
+  {
+    "revision": "e5575a7520a3a7374852",
+    "url": "/static/css/main.395bb43d.chunk.css"
+  },
+  {
+    "revision": "f0756375c976cb6ccb30",
+    "url": "/static/css/1.70d81161.chunk.css"
+  },
+  {
+    "revision": "07e44e668539b16824d3bd1eea4c8584",
+    "url": "/index.html"
+  }
+];
