f82c9a6f40f70e325a18870af45bb5fb51e812a1
Ricky Raykhenberg <ricky@optimizely.com>
2019-02-11 12:00:23 -0800

    Move data out of bundle.

---
+++ precache-manifest.436f772ef79fee9117b8e16fa06493b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "312ab5e16b05524e0987",
+    "url": "/sa-demo/static/js/runtime~main.312ab5e1.js"
+  },
+  {
+    "revision": "11c67c2a18198e8c245b",
+    "url": "/sa-demo/static/js/main.11c67c2a.chunk.js"
+  },
+  {
+    "revision": "8983ecbd3221d1bb13ef",
+    "url": "/sa-demo/static/js/1.8983ecbd.chunk.js"
+  },
+  {
+    "revision": "11c67c2a18198e8c245b",
+    "url": "/sa-demo/static/css/main.dc79b24d.chunk.css"
+  },
+  {
+    "revision": "8983ecbd3221d1bb13ef",
+    "url": "/sa-demo/static/css/1.5e530995.chunk.css"
+  },
+  {
+    "revision": "1f45ec1b86796497d1f359872ff7c919",
+    "url": "/sa-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.623be0d636ba18e401b569d2fa2658a6.js"
+  "/sa-demo/precache-manifest.436f772ef79fee9117b8e16fa06493b0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/sa-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
